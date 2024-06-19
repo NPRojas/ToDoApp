d6f52cb6a427819a63a7927f4a5aabb57442ff0a
Nicole Rojas <63315208+NPRojas@users.noreply.github.com>
2024-06-18 22:05:26 -0400

    build repository

--- app/src/main/java/com/example/todolistapp/data/repositories/ToDoRepository.kt
+++ app/src/main/java/com/example/todolistapp/data/repositories/ToDoRepository.kt
@@ -1,4 +1,14 @@
 package com.example.todolistapp.data.repositories
 
-class ToDoRepository {
+import com.example.todolistapp.data.db.ToDoDatabase
+import com.example.todolistapp.data.db.entities.ToDo
+
+class ToDoRepository(
+    private val db: ToDoDatabase
+) {
+    suspend fun update(item: ToDo) = db.getToDoDao().updateToDo(item)
+    suspend fun insert(item: ToDo) = db.getToDoDao().insertToDo(item)
+    suspend fun delete(item: ToDo) = db.getToDoDao().deleteToDo(item)
+
+    fun getAllToDos() = db.getToDoDao().getAllToDo()
 }
